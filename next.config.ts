0a417ed1d67c0847489029adc216a4120d998e3a
bos-8 <fox64.exe@gmail.com>
2026-04-13 22:57:07 +0200

    ACTIONS - v5

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,16 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin();
+
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const basePath = repositoryName ? `/${repositoryName}` : undefined;
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath,
+  assetPrefix: basePath,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
